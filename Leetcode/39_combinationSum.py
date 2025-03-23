892c0d627413df5a83593ef2200556c7376ff262
KHuang-dot-py <kaiwen.huang97@outlook.com>
2025-03-22 20:10:21 -0400

    Moving files around

--- Leetcode/39_combinationSum.py
+++ Leetcode/39_combinationSum.py
@@ -9,16 +9,12 @@ def combinationSum(candidates, target):
     solutions = []
     # return empty array if candidates is empty, no solutions
     if len(candidates) == 0: return solutions
-    
     #else, sort the array
     cand_sorted = sorted(candidates)
 
     def find_combo(array, target, parent_combo):
         if len(array) == 0: return
         for i in range(len(array)-1,-1,-1):
-            # child combo is a copy of parent combo
-            child_combo = copy.copy(parent_combo)
-            ### print("i: ", i)
         # if current greater than target, move to next element
             current = array[i]
             if current > target:
@@ -26,28 +22,37 @@ def combinationSum(candidates, target):
             else:
                 # if target is divisible by current element, this is a solution.
                 if target%current == 0:
-                    # extend child combo by num repeats
-                    child_combo.extend([current]*(target//current))
-                    # append solution to master array
-                    solutions.append(child_combo)
-
+                    solutions.append(parent_combo + [current]*(target//current))
                 # then, for each number of repeats of current up til greater than target
                 max_repeats = target//current
-                ### print("max_repeats", max_repeats)
-
+                # find combos for each number of repeats in the remaining array (barring current)
                 for j in range(1,max_repeats+1):
-                    # reset child_combo
-                    child_combo = copy.copy(parent_combo)
-                    # find combos for each number of repeats in the remaining array (barring current)
-                    ### print("j:", j)
-                    child_combo.extend([current]*j)
-                    find_combo(array[0:i], target - j*current, child_combo)
+                    find_combo(array[0:i], target - j*current, parent_combo + [current]*j)
     
     find_combo(candidates, target, [])
 
     return solutions
 
+
+def combinationSumDFS(candidates, target):
+    solutions = []
+    DFS(target, [], target)
+    # overall approach: explore all possibilities by exploring different paths, returning when the target is not achieved and
+    # ... sum of path is greater than the target
+    def DFS(target, path, remaining):
+        # base cases
+        if remaining < 0:
+            return
+        # solution is found when sum is exactly the target
+        if remaining == 0:
+            solutions.extend(path)
+        for i in range(0, len(candidates)):
+            DFS(target-candidates[i])
+
+
+
 a_rray = [2,3,6,7]
+print(a_rray+[3]*2)
 
 print(combinationSum(a_rray,7))
                          
