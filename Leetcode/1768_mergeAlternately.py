ee21a7db8b3201194ad2455a04778ba1deddbe6a
KHuang-dot-py <kaiwen.huang97@outlook.com>
2025-11-13 10:07:03 -0500

    improved solution for 1768

--- Leetcode/1768_mergeAlternately.py
+++ Leetcode/1768_mergeAlternately.py
@@ -1,17 +1,16 @@
-def mergeAlternately(word1, word2):
-    merged = ""
+# class Solution:
+def mergeAlternately(word1: str, word2: str) -> str:
     i = 0
     j = 0
-    while i + j < len(word1) + len(word2):
-        if i < len(word1) and (j == len(word2) or i == j):
-            merged = merged + word1[i]
+    result = []
+    while i+j < len(word1) + len(word2):
+        if i < len(word1):
+            result.append(word1[i])
             i += 1
-        else:
-            merged = merged + word2[j]
+        if j < len(word2):
+            result.append(word2[j])
             j += 1
-
-        print(i,j)
-    return merged
+    return "".join(result) 
 
 w1 = "abcdefg"
 w2 = "pqrs"
